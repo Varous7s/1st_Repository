f14a20d7ec28e48a43ecf76ef50c17e57c6a0375
Varous7 <36931911+Varous7s@users.noreply.github.com>
2019-07-27 20:06:07 +0530

    Add files via upload

---
+++ Passcode.java
@@ -0,0 +1,24 @@
+package com;
+
+public class Passcode {
+	int damage;
+	String password;
+	public Passcode(int damage, String password) {
+		this.damage = damage;
+		this.password = password;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
